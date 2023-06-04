a32df22016daf07574945363a5e0641159bc5502
ray hopf <rayhopf@protonmail.com>
2023-06-04 16:11:26 +0800

    update

--- ft-falcon-230604b.py
+++ ft-falcon-230604b.py
@@ -31,4 +31,26 @@
     --logging_steps=5 \
     --target_modules='["query_key_value"]'
 
+!wget https://huggingface.co/TheBloke/falcon-7b-instruct-GPTQ/resolve/main/gptq_model-4bit-64g.safetensors
+
+!falcontune finetune \
+    --model=falcon-7b-instruct-4bit \
+    --weights=gptq_model-4bit-64g.safetensors \
+    --dataset=./alpaca_data_cleaned.json \
+    --data_type=alpaca \
+    --lora_out_dir=./falcon-7b-instruct-4bit-alpaca/ \
+    --mbatch_size=1 \
+    --batch_size=2 \
+    --epochs=3 \
+    --lr=3e-4 \
+    --cutoff_len=256 \
+    --lora_r=8 \
+    --lora_alpha=16 \
+    --lora_dropout=0.05 \
+    --warmup_steps=5 \
+    --save_steps=50 \
+    --save_total_limit=3 \
+    --logging_steps=5 \
+    --target_modules='["query_key_value"]'
+
 '''
